65f1f35496047fea594f4b0b41c88622a914d57a
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-28 21:23:31 +0330

    fixed sequelize

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,9 +1,8 @@
-import {Sequelize} from 'sequelize-typescript';
+import {Sequelize} from 'sequelize';
 import {config} from '../config/index';
 
 const sequelize = new Sequelize(config.database.url ,{
     dialect:'postgres',
-    models:[__dirname + '/../modules/*/*.model.ts'],
     logging:config.app.env === 'development' ? console.log : false,
     pool:{
         max:5,
