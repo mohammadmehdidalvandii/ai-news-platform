a02d19debdac8ff3b5e14774fde1b058b1817864
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-07-06 15:39:27 +0330

    add package socks-proxy-agent

--- src/modules/telegram/bot.ts
+++ src/modules/telegram/bot.ts
@@ -1,4 +1,7 @@
 import {Telegraf } from 'telegraf'; 
+import {SocksProxyAgent} from 'socks-proxy-agent';
 import {config} from '../../config/index';
 
-export const bot = new Telegraf(config.telegram.token);
+const agent = new SocksProxyAgent('socks5://127.0.0.1:10808');
+
+export const bot = new Telegraf(config.telegram.token,{telegram:{agent}});  
